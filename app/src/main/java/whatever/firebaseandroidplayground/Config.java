c625cbeaa4802b484d96a7bea0cc6a3336a987fa
JanEberhardt <ebejan93@gmail.com>
2015-11-19 17:47:35 +0100

    tiny changes and urls

--- app/src/main/java/whatever/firebaseandroidplayground/Config.java
+++ app/src/main/java/whatever/firebaseandroidplayground/Config.java
@@ -5,5 +5,7 @@ package whatever.firebaseandroidplayground;
  */
 public class Config {
     public static String FIREBASE_BASE_ADDRESS = "https://savemya.firebaseio.com/";
-    public static String FIREBASE_LOCATION_TRACKING = FIREBASE_BASE_ADDRESS+"location_tracking/";
+    public static String FIREBASE_LOCATION_TRACKING = FIREBASE_BASE_ADDRESS+"locations/";
+    public static String GCM_REGISTER = "https://save-my-ass.appspot.com/_ah/api/savemyass/v1/register";
+    public static String GCM_START_ALARM = "https://save-my-ass.appspot.com/_ah/api/savemyass/v1/alarm";
 }
--- app/src/main/java/whatever/firebaseandroidplayground/LocationSaver.java
+++ app/src/main/java/whatever/firebaseandroidplayground/LocationSaver.java
@@ -14,9 +14,8 @@ public class LocationSaver {
     public static String TAG = "###LocationSaver";
     public static void saveLocation(Location loc, long timestamp, String userID){
         Firebase firebaseRef = new Firebase(Config.FIREBASE_LOCATION_TRACKING);
-        firebaseRef.child("last_update").child(userID).setValue(timestamp);
         GeoLocation gLoc = new GeoLocation(loc.getLatitude(), loc.getLongitude());
-        GeoFire geoFire = new GeoFire(firebaseRef.child("location"));
+        GeoFire geoFire = new GeoFire(firebaseRef);
         geoFire.setLocation(userID, gLoc);
     }
 
