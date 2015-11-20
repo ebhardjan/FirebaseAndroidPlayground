d8968a4b2fc632d288c6bdb9616c49b3b8ee6379
JanEberhardt <ebejan93@gmail.com>
2015-11-20 22:30:20 +0100

    if no internet is available sending gets postponed until there is

--- app/src/main/java/whatever/firebaseandroidplayground/LocationTrackerService.java
+++ app/src/main/java/whatever/firebaseandroidplayground/LocationTrackerService.java
@@ -3,10 +3,13 @@ package whatever.firebaseandroidplayground;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.location.Location;
-import android.location.LocationListener;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.Handler;
@@ -23,6 +26,7 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -37,6 +41,8 @@ public class LocationTrackerService extends Service implements
     private Timer timer;
     private Handler handler = new Handler();
     private final IBinder binder = new LocalBinder();
+    private ConnectivityManager cm;
+    private ArrayList<String> log;
 
     // the ID of the user
     public String userID = "asdfasdf";
@@ -66,7 +72,7 @@ public class LocationTrackerService extends Service implements
     public void onCreate() {
         super.onCreate();
 
-        // todo: get the userID!!!!
+        // todo: get the userID!!!! -> probably do this in the main activity and put it here...
         userID = "t"+System.currentTimeMillis();
 
         // set firebase android context
@@ -76,7 +82,12 @@ public class LocationTrackerService extends Service implements
         buildGoogleApiClient();
         mGoogleApiClient.connect();
 
-        showNotification();
+        log = new ArrayList<>();
+
+        log("service created");
+
+        // connectivity manager initialization, used for hasInternet
+        cm = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
 
         // timer initialization and run tasks periodically
         if(timer == null)
@@ -85,6 +96,11 @@ public class LocationTrackerService extends Service implements
             timer.cancel();
         timer.scheduleAtFixedRate(new PeriodicalTask(), 0, Config.LOCATION_UPDATE_PERIOD);
 
+    }
+
+    private String getTime(){
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        return sdf.format(new Date())+": ";
     }
 
     protected synchronized void buildGoogleApiClient() {
@@ -103,18 +119,17 @@ public class LocationTrackerService extends Service implements
         return mLocationRequest;
     }
 
-
-    // note: if ID stays unchanged it just updates the notification...
-    private void showNotification() {
+    /**
+     * Displays notification for debugging and testing purposes...
+     * note: if ID stays unchanged it just updates the notification...
+     */
+    private void showNotification(String text) {
         // initialize the notification
-        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-        String currentDateandTime = sdf.format(new Date());
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(getApplicationContext())
                         .setSmallIcon(R.mipmap.ic_launcher)
                         .setContentTitle("Location Tracker Service")
-                        .setContentText("running: "+currentDateandTime);
-
+                        .setContentText(getTime() + ": " + text);
         Intent resultIntent = new Intent(getApplicationContext(), MainActivity.class);
         PendingIntent resultPendingIntent =
                 PendingIntent.getActivity(
@@ -128,11 +143,24 @@ public class LocationTrackerService extends Service implements
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         //  allows you to update the notification later on.
         mNotificationManager.notify(notificationID, mBuilder.build());
+        log(text);
     }
 
+    private void log(String text){
+        log.add(getTime()+text);
+    }
 
+    public ArrayList<String> getLog(){
+        return log;
+    }
 
     // does things periodically
+
+    /**
+     * Task that gets executed periodically, currently does just nothing except showing the
+     * notification...
+     * Will this really be used later on? -> I think no!
+     */
     class PeriodicalTask extends TimerTask {
 
         @Override
@@ -142,10 +170,77 @@ public class LocationTrackerService extends Service implements
 
                 @Override
                 public void run() {
-                    showNotification();
+                    log("PeriodicalTask run()");
                 }
             });
         }
+    }
+
+    /**
+     * Checks if the location is far enough away from the last one sent, and sends it in that case
+     */
+    private void checkLocationAndUpdate(Location lastLocation) {
+        log("checking if updating location in firebase is needed");
+        Location loggedLocation;
+        if (lastLocation != null) {
+            loggedLocation = lastLocation;
+            if (lastSentLocation == null)
+                sendLocation(loggedLocation);
+            else if (loggedLocation.distanceTo(lastSentLocation) > Config.LOCATION_TRACKER_SEND_DISTANCE_TRESHOLD)
+                //send current position
+                sendLocation(loggedLocation);
+        }
+    }
+
+    /**
+     * Saves location in firebase
+     */
+    private void sendLocation(Location loc){
+        if(hasInternet()) {
+            log("updating location in firebase...");
+            // save loc in firebase
+            Firebase firebaseRef = new Firebase(Config.FIREBASE_LOCATION_TRACKING);
+            GeoLocation gLoc = new GeoLocation(loc.getLatitude(), loc.getLongitude());
+            GeoFire geoFire = new GeoFire(firebaseRef);
+            geoFire.setLocation(userID, gLoc);
+        }
+        else {
+            log("currently no internet connection, registering connectivity_change receiver");
+            //register broadcast receiver and do this when internet is available again...
+            IntentFilter filter = new IntentFilter("android.net.conn.CONNECTIVITY_CHANGE");
+            MyReceiver myReceiver = new MyReceiver(loc);
+            registerReceiver(myReceiver, filter);
+        }
+    }
+
+    /**
+     * Broadcast receiver for the internet connectivity event...
+     */
+    public class MyReceiver extends BroadcastReceiver {
+        private Location loc;
+        public MyReceiver(Location log) {}
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // This method is called when this BroadcastReceiver receives an Intent broadcast.
+            if(hasInternet())
+                log("internet available -> logging");
+                // save loc in firebase
+                Firebase firebaseRef = new Firebase(Config.FIREBASE_LOCATION_TRACKING);
+                GeoLocation gLoc = new GeoLocation(loc.getLatitude(), loc.getLongitude());
+                GeoFire geoFire = new GeoFire(firebaseRef);
+                geoFire.setLocation(userID, gLoc);
+        }
+    }
+
+    /**
+     * Returns true if internet is available
+     */
+    protected boolean hasInternet(){
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+        return isConnected;
     }
 
     /**
@@ -157,35 +252,6 @@ public class LocationTrackerService extends Service implements
         LocationServices.FusedLocationApi.requestLocationUpdates(
                 mGoogleApiClient, createLocationRequest(), this);
         checkLocationAndUpdate(LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient));
-    }
-
-    /**
-     * Checks if the location is far enough away from the last one sent, and sends it in that case
-     * @param lastLocation
-     */
-    private void checkLocationAndUpdate(Location lastLocation) {
-        Log.d(TAG, "checking if updating location in firebase is needed");
-        Location loggedLocation;
-        if (lastLocation != null) {
-            loggedLocation = lastLocation;
-            if (lastSentLocation == null)
-                sendLocation(loggedLocation);
-            else if (loggedLocation.distanceTo(lastSentLocation) > Config.LOCATION_TRACKER_SEND_DISTANCE_TRESHHOLD)
-                //send current position
-                sendLocation(loggedLocation);
-        }
-    }
-
-    /**
-     * Saves location in firebase
-     * @param loc
-     */
-    private void sendLocation(Location loc){
-        Log.d(TAG, "updating location in firebase...");
-        Firebase firebaseRef = new Firebase(Config.FIREBASE_LOCATION_TRACKING);
-        GeoLocation gLoc = new GeoLocation(loc.getLatitude(), loc.getLongitude());
-        GeoFire geoFire = new GeoFire(firebaseRef);
-        geoFire.setLocation(userID, gLoc);
     }
 
     @Override
@@ -200,13 +266,13 @@ public class LocationTrackerService extends Service implements
     public void onConnectionSuspended(int cause) {
         // The connection to Google Play services was lost for some reason. We call connect() to
         // attempt to re-establish the connection.
-        Log.i(TAG, "Connection to Google Play services suspended");
+        Log.d(TAG, "Connection to Google Play services suspended");
         mGoogleApiClient.connect();
     }
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
-        Log.i(TAG, "Connection to Google Play services failed");
+        Log.d(TAG, "Connection to Google Play services failed");
         mGoogleApiClient.connect();
     }
 
@@ -215,14 +281,8 @@ public class LocationTrackerService extends Service implements
         return binder;
     }
 
-    /**
-     * LocationListener overrides-------------------------------------------------------------------
-     */
     @Override
     public void onLocationChanged(Location location) {
         checkLocationAndUpdate(location);
     }
-    /**
-     * ---------------------------------------------------------------------------------------------
-     */
 }
